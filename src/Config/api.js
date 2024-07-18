2ca2fdd0d6922e022a5701cfdbef6118f99c1416
Your Name <tarachandyadav018@gmail.com>
2024-07-19 00:10:00 +0530

    connect with backend

--- src/Config/api.js
+++ src/Config/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-export const API_BASE_URL = "http://localhost:8080";
+// export const API_BASE_URL = "http://localhost:8080";
+export const API_BASE_URL = "jarvis-server-production.up.railway.app";
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
